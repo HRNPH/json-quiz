ba35b8719c665ef633a3f4ae6cbe6f7a49d068fa
HRNPH <hrnph@protonmail.com>
2025-11-10 19:00:04 +0700

    feat: Stuff

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
       },
     }),
   ],
-  base: process.env.NODE_ENV === "production" ? "/json-exam/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/json-quiz/" : "/",
 });
